2723d38ca9854534897ac18b182d5cae9ba398cd
adilkenzh17 <95138445+neobyvatel@users.noreply.github.com>
2023-02-10 19:11:48 +0600

    add class Flights

---
+++ src/Flights.java
@@ -0,0 +1,7 @@
+public class Flights {
+        private int id;
+        private static int id_n = 1;
+        private String destination;
+        private boolean FlightType;
+        private int price;
+}
