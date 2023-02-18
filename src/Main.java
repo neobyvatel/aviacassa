1b979890729158ca50c09705b267280460ce1828
t0rch13 <batyrhan22.11@gmail.com>
2023-02-19 02:30:11 +0600

    feat: UserFlightRepository class added, implementation of getASeat method inside this class

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import controllers.FlightController;
 import data.PostgresDB;
 import data.interfaces.IDB;
 import repositories.FlightRepository;
+import repositories.UserFlightRepository;
 import repositories.UserRepository;
 import repositories.interfaces.IFlightRepository;
 import repositories.interfaces.IUserRepository;
@@ -14,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         IDB db = new PostgresDB();
-        IUserRepository userRepo = new UserRepository(db);
+        IUserRepository userRepo = new UserFlightRepository(db);
         IFlightRepository flightRepo = new FlightRepository(db);
         UserController userController = new UserController(userRepo);
         FlightController flightController = new FlightController(flightRepo);
--- src/repositories/UserRepository.java
+++ src/repositories/UserRepository.java
@@ -11,8 +11,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
 
-public class UserRepository implements IUserRepository {
-    private final IDB db;
+public abstract class UserRepository implements IUserRepository {
+    private IDB db;
+    public UserRepository(){
+    }
 
     public UserRepository(IDB db) {
         this.db = db;
@@ -82,67 +84,6 @@ public class UserRepository implements IUserRepository {
 
     }
 
-     @Override 
-    public boolean getASeat(User user, Flight flight) {
-
-        Connection con = null;
-
-        int seats = flight.getSeats();
-        int price = flight.getPrice();
-        int balance = user.getBalance();
-
-        int userId = user.getId();
-        int flightId = flight.getId();
-
-        ArrayList<Flight> flights = user.getFlights();
-
-        int remaining = balance - price;
-        int updatedSeats = seats - 1;
-
-        String flightOrigDest = flight.getOrigDest();
-
-         if (seats > 0) {
-             if (remaining > 0) {
-                flight.setSeats(updatedSeats);
-                user.setBalance(remaining);
-                flights.add(flight);
-                user.setFlights(flights);
-             } else System.out.println("Insufficient balance");
-         }else System.out.println("No seats avalable!");
-
-         try {
-             con = db.getConnection();
-             String UserSQL = "UPDATE users SET balance = ?, flights = ? WHERE user_id = ?";
-             PreparedStatement st = con.prepareStatement(UserSQL);
-
-             st.setInt(1, remaining);
-             st.setString(2,flightOrigDest);
-             st.setInt(3, userId);
-
-             st.execute();
-
-             String flightSQL = "UPDATE flights SET seats = ? WHERE flight_id = ?";
-             PreparedStatement flightST = con.prepareStatement(flightSQL);
-             flightST.setInt(1,updatedSeats);
-             flightST.setInt(2, flightId);
-
-             flightST.execute();
-
-             return true;
-         } catch (SQLException throwables) {
-             throwables.printStackTrace();
-         } catch (ClassNotFoundException e) {
-             e.printStackTrace();
-         } finally {
-             try {
-                 con.close();
-             } catch (SQLException throwables) {
-                 throwables.printStackTrace();
-             }
-         }
-         return false;
-
-    }
 
     @Override
     public User getUser(int id) {
@@ -212,5 +153,10 @@ public class UserRepository implements IUserRepository {
             }
         }
         return null;
+    }
+
+    @Override
+    public boolean addBalance() {
+        return false;
     }
 }
