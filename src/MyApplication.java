e3d26daa01ea9afa5861b5d5db3cbfaf5540fc6b
adilkenzh17 <95138445+neobyvatel@users.noreply.github.com>
2023-02-19 17:44:04 +0600

    add password strength checker

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -26,7 +26,8 @@ public class MyApplication {
         System.out.println("\\033[H\\033[2J");
         System.out.flush();
     }
-
+    public static final Pattern PASSWORD_STRENGTH_REGEX = Pattern
+            .compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[/S]{8,10}$");
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
             Pattern.CASE_INSENSITIVE);
     public void getAllUsersMenu() {
