8c38d2870525617bd880dd240d981407096c26a2
adilkenzh17 <95138445+neobyvatel@users.noreply.github.com>
2023-02-19 19:12:44 +0600

    fix and modify addBalance method in User.java

--- src/entities/User.java
+++ src/entities/User.java
@@ -19,9 +19,8 @@ public class User {
         setBalance(balance);
 
     }
-    public int addBalance(int Balance, int amountToAdd) {
-        int newBalance = balance + amountToAdd;
-        return newBalance;
+    public int addBalance(int balance, int amountToAdd) {
+        return(balance+amountToAdd);
     }
     public User(int id, String name, String surname, String email, int balance) {
         this(name, surname,email, balance);
