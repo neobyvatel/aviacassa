6f54da06a6ec48107fa32d9aa3b26292f14cd9ab
adilkenzh17 <95138445+neobyvatel@users.noreply.github.com>
2023-02-25 13:03:09 +0600

    remove getAllUsersMenu

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -30,10 +30,7 @@ public class MyApplication {
             .compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[/S]{8,10}$");
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
             Pattern.CASE_INSENSITIVE);
-    public void getAllUsersMenu() {
-        String response = userController.getAllUsers();
-        System.out.println(response);
-    }
+
 
     public static boolean validateEmail(String email) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
